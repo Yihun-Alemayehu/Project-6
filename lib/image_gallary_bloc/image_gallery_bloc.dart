d0c927b2860b4c2f3c84e8494cda2633575689e9
Yihun <yankure01@gmail.com>
2024-02-19 11:29:59 +0300

    fdb-04

--- lib/image_gallary_bloc/image_gallery_bloc.dart
+++ lib/image_gallary_bloc/image_gallery_bloc.dart
@@ -1,14 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:project_6/data/models/image_model.dart';
+import 'package:http/http.dart';
 
 part 'image_gallery_event.dart';
 part 'image_gallery_state.dart';
 
+final http = Client();
+
 class ImageGalleryBloc extends Bloc<ImageGalleryEvent, ImageGalleryState> {
   ImageGalleryBloc() : super(ImageGalleryInitial()) {
-    on<ImageGalleryEvent>((event, emit) {
-      // TODO: implement event handler
+    on<LoadImageGallery>((event, emit) async {
+      emit(ImageLoadingState());
+      try {
+        final response = await http.get(
+          Uri.parse(
+              'https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY'),
+        );
+      } catch (e) {}
     });
   }
 }
